183c63f87043406f6e97e3b3cb5ca5afb4cd0f31
ansaroyan <anoushsaroyan@gmail.com>
2019-10-08 09:38:46 -0700

    justy

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 const ProjectType = require("./project_type")
 const UserType = require("./user_type");
 const TeamType = require("./team_type")
@@ -13,6 +13,7 @@ const Task = mongoose.model("task");
 const Team = mongoose.model("team");
 const Message = mongoose.model("message");
 const Chat = mongoose.model("chat");
+const AuthService = require('../../services/auth');
 
 const RootQueryType = new GraphQLObjectType({
     name: "RootQueryType",
@@ -29,13 +30,6 @@ const RootQueryType = new GraphQLObjectType({
             resolve(_, args) {
                 return User.findById(args._id);
             }
-        },
-        userByEmail: {
-          type: UserType,
-          args: { email: { type: new GraphQLNonNull(GraphQLString) } },
-          resolve(_, args) {
-            return User.findOne(args);
-          }
         },
         projects: {
             type: new GraphQLList(ProjectType),
@@ -102,6 +96,29 @@ const RootQueryType = new GraphQLObjectType({
                 return Chat.findById(args._id);
             }
         },
+        userChats: {
+            type: new GraphQLList(ChatType),
+            args: { _id: { type: new GraphQLNonNull(GraphQLID) } }, // user_id
+            async resolve(_, args, context) {
+                let validUser = await AuthService.verifyUser({ token: context.token });
+                let conversations = await Chat.find({});
+                if (validUser.loggedIn) {
+                    return conversations.filter(chat => chat.users.includes(args._id))
+                } else {
+                    throw new Error("Sorry, you need to be logged in to create a product");
+                }
+            }
+            // async resolve(parentValue, { name, description, weight }, context) {
+            //     const validUser = await AuthService.verifyUser({ token: context.token });
+
+            //     if (validUser.loggedIn) {
+            //         const user = validUser._id;
+            //         return new Product({ name, description, weight, user }).save();
+            //     } else {
+            //         throw new Error("Sorry, you need to be logged in to create a product");
+            //     }
+            // }
+        }
     })
 });
 
