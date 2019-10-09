533d03b027fb75a33368f97bf7605c4cf1b77c32
Carlos Catly <ccatly@gmail.com>
2019-10-08 23:47:40 -0700

    The team submenu in the sidebar looks decent now

--- client/src/components/sidebar/TeamSubMenu.js
+++ client/src/components/sidebar/TeamSubMenu.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "./teamsubmenu.scss";
+import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
+import { FaSquare } from "react-icons/fa";
+
 
 
 class TeamSubMenu extends Component {
@@ -21,31 +24,79 @@ class TeamSubMenu extends Component {
 
     renderTeamMembers(team){
         let members
-        members = team.users.map(user => <div><Link>{user.name}</Link></div>)
+        members = team.users.map(user => {
+            const abbreviatedName = user.name.split(" ").map(word => word[0])
+            let rightLetters
+            if (abbreviatedName.length === 1) {
+                rightLetters = abbreviatedName[0]
+            } else {
+                rightLetters = [abbreviatedName[0] + abbreviatedName[abbreviatedName.length - 1]]
+            }
+
+            let color
+            user.color ? color = user.color : color = "#e362e3"
+
+            let profileColor = {
+                backgroundColor: color
+            }
+            return (
+            <div className="main-header-avatar-pic" style={profileColor} key={user._id}>
+                {rightLetters}
+            </div>)})
         return(
             <div>
-                {members}
+                <div className="team-submenu-people">
+                    <div className="team-submenu-circles">{members}</div>
+                    <div className="submenu-invite-people">Invite People</div>
+                </div>
             </div>
         )
     }
 
     renderProjects(team){
         let projects
-        projects = team.projects.map(project => <div><Link>{project.name}</Link></div>)
+    projects = team.projects.map(project => {
+        let color
+        project.color ? color = project.color : color = "#e362e3"
+
+        let projectColor = {
+            color: color
+        }
+    return (
+    <div className="team-submenu-project" key={project._id}><Link><FaSquare style={projectColor}/>{project.name}</Link></div>
+    )
+    })
         return(
-            <div>
+            <div className="team-submenu-projects">
                 {projects}
             </div>
         )
     }
 
+    handleSubMenu(team){
+        if (this.state.open) {
+            return (
+                <div className="team-submenu">
+                    <div className="team-submenu-title noselect"><TiArrowSortedDown onClick={this.handleCollapse("open")}/><Link className="submenu-team-name">{team.name}</Link></div>
+                    {this.renderTeamMembers(team)}
+                    {this.renderProjects(team)}
+                </div>
+            )
+        } else {
+            return (
+                <div className="team-submenu">
+                    <div className="team-submenu-title noselect"><TiArrowSortedUp onClick={this.handleCollapse("open")}/><Link className="submenu-team-name">{team.name}</Link></div>
+                </div>
+            )
+        }
+    }
+
+
+
+
     render(){
         const { team } = this.props
-        return<div>
-            <Link className="submenu-team-name">{team.name}</Link>
-            {this.renderTeamMembers(team)}
-            {this.renderProjects(team)}
-        </div>
+        return this.handleSubMenu(team)
     }
 }
 
